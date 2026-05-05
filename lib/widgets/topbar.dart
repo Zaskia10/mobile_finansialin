6ba0885744238b9cc67980bd794f0e9225788ff0
Zaskia Khairani <zkhairani33@gmail.com>
2026-05-05 22:57:35 +0700

    integrasi ocr dan chatbot

--- lib/widgets/topbar.dart
+++ lib/widgets/topbar.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
   final List<Widget>? actions;
+  final bool showBackButton;
 
-  const CustomAppBar({Key? key, required this.title, this.actions}) : super(key: key);
+  const CustomAppBar({
+    Key? key, 
+    required this.title, 
+    this.actions,
+    this.showBackButton = true,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,10 +18,12 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
       backgroundColor: Colors.white,
       elevation: 0, 
       centerTitle: true,
-      leading: IconButton(
-        icon: const Icon(Icons.arrow_back, color: Colors.black),
-        onPressed: () => Navigator.pop(context),
-      ),
+      leading: showBackButton 
+        ? IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.black),
+            onPressed: () => Navigator.pop(context),
+          )
+        : null,
       title: Text(
         title,
         style: const TextStyle(
